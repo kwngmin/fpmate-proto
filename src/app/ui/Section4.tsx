8fc44e7a92db3b2989201279d4bfc9c8d1981732
kwngmin <kwngmink@gmail.com>
2025-11-27 14:14:18 +0900

    section4 새로 만든 리플렛 내용 반영

--- src/app/ui/Section4.tsx
+++ src/app/ui/Section4.tsx
@@ -1,44 +1,149 @@
-import { Card, Typography } from "@/shared/ui";
+import Image from "next/image";
+import { Fragment } from "react/jsx-runtime";
 
 /**
  * Section 4 서비스 카드 데이터
  */
-const section4Contents = [
+const Contents = [
   {
     id: 1,
-    key: "function",
-    title: "SW 사업 기획자",
-    subTitle: "SW 사업 비용 알아보기",
-    sectionTitle: ["SW 개발비/재개발비", "SW 유지관리비"],
-    tags: ["AI로 쉽게", "AI로 빠르게", "더 똑똑하게"],
-    values: [
-      "축척된 데이터를 활용한 AI 추천/자동화",
-      "신기술 적용하여 비용산정 정확도 향상",
+    key: "ai-recommendation",
+    title: "AI 추천 기능",
+    icon: "robot",
+    listItems: [
+      {
+        title: "업무 및 기능 식별 자동화",
+        description: "업무 및 기능 추천",
+      },
+      {
+        title: "기능 유형 판단 자동화",
+        description: "EI, EO, EQ, ILF 등 기능 유형 추천",
+      },
     ],
-    color: "red",
   },
   {
     id: 2,
-    key: "function",
-    title: "개발자/운영자",
-    subTitle: "SW 사업 관리하기",
-    sectionTitle: ["SW 사업 진행 관리", "SW 변경 관리"],
-    tags: ["프로젝트 현황을 한눈에", "비교 / 추적 / 변경"],
-    values: ["상세한 개발규모 파악", "기능 변경에 따른 변경 비용 관리"],
-    color: "blue",
+    key: "fp-automation",
+    title: "FP산정 자동화",
+    icon: "arrows-clockwise",
+    listItems: [
+      {
+        title: "산정 유형에 따른 기능 점수 자동 계산",
+        description: null,
+      },
+      {
+        title: "업무별 기능 점수 합계 자동 계산",
+        description: null,
+      },
+      {
+        title: "FP 측정 결과 즉시 확인",
+        description: null,
+      },
+      {
+        title: "FP 기반 SW 사업비용 산정",
+        description: null,
+      },
+    ],
   },
   {
     id: 3,
-    key: "function",
-    title: "SW 사업 관리자",
-    subTitle: "SW 사업 관리 +",
-    sectionTitle: ["인력 관리", "솔루션 관리", "일감 관리"],
-    tags: ["사업에 필요한 모든 것", "All in One SW 사업 관리"],
-    values: [
-      "SW 사업 발주사/수주사 누구나",
-      "업체 규모 상관없이 SW 사업에 필요한 FPMate",
+    key: "data-verification",
+    title: "데이터 검증",
+    icon: "list-checks",
+    listItems: [
+      {
+        title: "중복검사",
+        description: null,
+      },
+      {
+        title: "명칭 오류 검사",
+        description: null,
+      },
+      {
+        title: "속성 미작성 검사",
+        description: null,
+      },
+      {
+        title: "기능 유형 판단 오류 검증",
+        description: null,
+      },
     ],
-    color: "green",
+  },
+  {
+    id: 4,
+    key: "guidance",
+    title: "오류 수정 가이드",
+    icon: "warning",
+    listItems: [
+      {
+        title: "오류 유형별 수정 가이드",
+        description: null,
+      },
+      {
+        title: "확인 필요 사항에 대한 쉬운 탐색",
+        description: null,
+      },
+      {
+        title: "정확한 산정 유도",
+        description: null,
+      },
+    ],
+  },
+  {
+    id: 5,
+    key: "report",
+    title: "산정결과 리포트",
+    icon: "newspaper",
+    listItems: [
+      {
+        title: "산정 내역 축척",
+        description: null,
+      },
+      {
+        title: "산정 내역 접근 권한 관리",
+        description: null,
+      },
+      {
+        title: "비용 산정 진행 관리",
+        description: null,
+      },
+      {
+        title: "산정 보고서",
+        description: null,
+      },
+      {
+        title: "산정 내역 분석",
+        description: null,
+      },
+    ],
+  },
+  {
+    id: 6,
+    key: "data-utilization",
+    title: "데이터 활용",
+    icon: "database",
+    listItems: [
+      {
+        title: "산정 결과 재활용",
+        description: null,
+      },
+      {
+        title: "산정 데이터를 활용한 추천",
+        description: null,
+      },
+      {
+        title: "산정 결과 통계 및 시각화",
+        description: null,
+      },
+      {
+        title: "산정결과 비교 기능 제공",
+        description: null,
+      },
+      {
+        title: "경영 인사이트 제공",
+        description: null,
+      },
+    ],
   },
 ];
 
@@ -48,73 +153,77 @@ const Section4 = () => {
       <div className="max-w-[1200px] mx-auto px-6 flex flex-col gap-6">
         <div className="flex flex-col gap-4">
           <div className="text-[2rem] md:text-[3.5rem] leading-tight tracking-tighter font-semibold break-keep text-center text-text-primary">
-            진화하는{" "}
-            <span className="font-extrabold text-brand-primary">FPMate</span>,{" "}
-            <br className="sm:hidden" />
-            SW 사업 성공 도우미
+            <span className="font-extrabold text-brand-primary">FPMate</span>{" "}
+            주요 기능
           </div>
-          <p className="text-[1.3125rem] md:text-[2rem]  leading-snug tracking-tighter break-keep text-center text-text-primary">
-            당신의 <span className="font-semibold">SW 사업 성공</span>과{" "}
-            <span className="font-semibold">Fine 프로젝트</span>를 위하여{" "}
-            <br className="hidden sm:block" />
-            <span className="font-bold text-brand-primary">FPMate</span>는
-            지속적으로 진화합니다.
-          </p>
+
+          {/* icons */}
+          <div className="flex justify-center flex-wrap gap-2">
+            {Contents.map((content) => (
+              <div
+                key={content.id}
+                className="rounded-full bg-white p-2 border border-border-primary"
+              >
+                <Image
+                  src={`/assets/svgs/${content.icon}.svg`}
+                  alt={content.title}
+                  width={100}
+                  height={100}
+                  className="size-6"
+                />
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 my-6">
-          {section4Contents.map((content) => (
-            <Card
-              key={content.id}
-              variant="bordered"
-              hoverable
-              padding="none"
-              className="shrink-0 overflow-hidden w-full max-w-md mx-auto md:max-w-none"
-            >
-              <div className="h-4 bg-gray-100" />
-              <div className="flex flex-col gap-6 px-6 pt-8 pb-16">
-                {/* <div className="w-12 h-12 rounded-full bg-gray-100" /> */}
-                <Typography variant="title3">{content.title}</Typography>
-                <div className="flex flex-col gap-2">
-                  <Typography variant="title2">{content.subTitle}</Typography>
-                  <div className="flex flex-col gap-1">
-                    {content.sectionTitle.map((title) => (
-                      <div key={title} className="flex items-center gap-2">
-                        <div className="w-1 h-7 rounded-full bg-gray-200" />
-                        <span className="text-[1.0625rem] sm:text-[0.9375rem] font-medium">
-                          {title}
-                        </span>
-                      </div>
-                    ))}
-                  </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-6 my-6">
+          {Contents.map((content, index) => {
+            const backgroundColor =
+              (index + 1) % 3 === 0
+                ? "bg-green-600"
+                : (index + 1) % 3 === 1
+                ? "bg-lime-600"
+                : "bg-teal-600";
+
+            return (
+              <div
+                className="flex flex-col gap-2 h-full shrink-0 overflow-hidden w-full max-w-sm mx-auto md:max-w-none"
+                key={content.id}
+              >
+                <div
+                  className={`relative text-white font-semibold h-11 px-6 flex justify-center items-center rounded shadow-md ${backgroundColor}`}
+                >
+                  {content.title}
+                  <div
+                    className="absolute -bottom-1.5 left-1/2 -translate-x-1/2 w-0 h-0"
+                    style={{
+                      borderLeft: "6px solid transparent",
+                      borderRight: "6px solid transparent",
+                      borderTop: `6px solid green`,
+                    }}
+                  />
                 </div>
-                <ul className="flex  flex-wrap gap-1">
-                  {content.tags.map((tag) => (
+                <ul className="flex flex-col gap-1 px-6 py-10 bg-gray-50 grow border border-border-primary rounded">
+                  {content.listItems.map((item) => (
                     <li
-                      key={tag}
-                      className="text-base sm:text-[0.875rem] px-3 h-8 sm:h-7 rounded-sm bg-gray-100 flex items-center font-medium"
+                      key={item.title}
+                      className="text-[0.9375rem] leading-snug tracking-tight break-keep list-disc ml-4 text-text-primary"
                     >
-                      {tag}
+                      {item.title}
+                      {item?.description && (
+                        <Fragment>
+                          <br />
+                          <span className="text-[0.875rem] text-text-secondary">
+                            ({item.description})
+                          </span>
+                        </Fragment>
+                      )}
                     </li>
                   ))}
                 </ul>
-
-                <div className="flex flex-col gap-2">
-                  <span>제공가치</span>
-                  <ul className="flex flex-col gap-1 pl-2">
-                    {content.values.map((value) => (
-                      <li
-                        key={value}
-                        className="text-[1.0625rem] sm:text-[0.9375rem]  font-medium break-keep list-disc ml-4"
-                      >
-                        {value}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
               </div>
-            </Card>
-          ))}
+            );
+          })}
         </div>
       </div>
     </section>
